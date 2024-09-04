7b3419f9c1a3848cc75fedb93f57c25419bca516
aRoH2121 <ale1002@outlook.it>
2024-09-04 23:28:19 +0200

    ultimi aggiustamenti v4

--- myapp/tests.py
+++ myapp/tests.py
@@ -5,40 +5,21 @@ from .models import *
 class UtenteModelTestCase(TestCase):
     def setUp(self):
         self.dottore = Utente.objects.create_user(
-            username='dottore', 
-            email='dottore@example.com', 
-            password='password',
-            tipo_utente=True,  # Dottore
-            cf='DTTDRT70A01F205Z',
-            nome='Mario',
-            cognome='Rossi',
-            provincia='RM',
-            telefono='3331234567',
-            genere='M',
-            data_nascita='1970-01-01',
+            username='dottore', email='dottore@example.com', password='password',tipo_utente=True,  # Dottore
+            cf='DTTDRT70A01F205Z',nome='Mario',cognome='Rossi',provincia='RM',telefono='3331234567',genere='M',data_nascita='1970-01-01',
             foto_profilo='avatar-dottore.png'
         )
 
-        self.paziente = Utente.objects.create_user(
-            username='paziente', 
-            email='paziente@example.com', 
-            password='password',
-            tipo_utente=False, 
-            cf='PZNTST80A01F205Z',
-            nome='Luigi',
-            cognome='Bianchi',
-            provincia='MI',
-            telefono='3337654321',
-            genere='M',
-            data_nascita='1980-01-01',
+        self.paziente = Utente.objects.create_user(username='paziente', email='paziente@example.com', password='password',tipo_utente=False,#Paziente
+            cf='PZNTST80A01F205Z',nome='Luigi',cognome='Bianchi',provincia='MI',telefono='3337654321',genere='M',data_nascita='1980-01-01',
             foto_profilo='avatar-paziente.png'
         )
 
-    def test_creazione_utenti(self):
+    def UserCreationTest(self):
         self.assertTrue(self.dottore.tipo_utente)
         self.assertFalse(self.paziente.tipo_utente)
 
-    def test_creazione_cura(self):
+    def CuraCreationTest(self):
         cura = Cura.objects.create(
             idDottore=self.dottore, 
             idPaziente=self.paziente, 
@@ -47,7 +28,7 @@ class UtenteModelTestCase(TestCase):
         self.assertEqual(cura.idDottore, self.dottore)
         self.assertEqual(cura.idPaziente, self.paziente)
 
-    def test_cura_con_utenti_invalidi(self):
+    def InvalidUserTest(self):
         self.paziente.tipo_utente = True
         self.paziente.save()
         with self.assertRaises(ValidationError):
@@ -63,49 +44,29 @@ class UtenteModelTestCase(TestCase):
 
 class ChatMessageTestCase(TestCase):
     def setUp(self):
-        self.utente1= Utente.objects.create_user(
-            username='dottore', 
-            email='dottore@example.com', 
-            password='password',
-            tipo_utente=True,  # Dottore
-            cf='DTTDRT70A01F205Z',
-            nome='Mario',
-            cognome='Rossi',
-            provincia='RM',
-            telefono='3331234567',
-            genere='M',
-            data_nascita='1970-01-01',
-            foto_profilo='avatar-dottore.png'
+        self.utente1= Utente.objects.create_user(username='dottore', email='dottore@example.com', password='password',tipo_utente=True,  # Dottore
+            cf='DTTDRT70A01F205Z',nome='Mario',cognome='Rossi',provincia='RM',telefono='3331234567',genere='M',
+            data_nascita='1970-01-01',foto_profilo='avatar-dottore.png'
         )
 
-        self.utente2 = Utente.objects.create_user(
-            username='paziente', 
-            email='paziente@example.com', 
-            password='password',
-            tipo_utente=False,  # Paziente
-            cf='PZNTST80A01F205Z',
-            nome='Luigi',
-            cognome='Bianchi',
-            provincia='MI',
-            telefono='3337654321',
-            genere='M',
-            data_nascita='1980-01-01',
-            foto_profilo='avatar-paziente.png'
+        self.utente2 = Utente.objects.create_user(username='paziente', email='paziente@example.com', password='password',tipo_utente=False,  # Paziente
+            cf='PZNTST80A01F205Z',nome='Luigi',cognome='Bianchi',provincia='MI',telefono='3337654321',genere='M',
+            data_nascita='1980-01-01',foto_profilo='avatar-paziente.png'
         )
         self.chat = Chat.objects.create()
         self.chat.participants.set([self.utente1, self.utente2])
     
-    def test_creazione_chat(self):
+    def ChatCreationTest(self):
         self.assertIn(self.utente1, self.chat.participants.all())
         self.assertIn(self.utente2, self.chat.participants.all())
 
-    def test_creazione_message(self):
+    def MessageCreationTest(self):
         messaggio = Message.objects.create(chat=self.chat, sender=self.utente1, content="Ciao!")
         self.assertEqual(messaggio.chat, self.chat)
         self.assertEqual(messaggio.sender, self.utente1)
         self.assertEqual(messaggio.content, "Ciao!")
     
-    def test_visualizzazione_messaggio(self):
+    def MessagevisualizationTest(self):
         messaggio = Message.objects.create(chat=self.chat, sender=self.utente1, content="Ciao!")
         serialized_message = messaggio.serialize()
         self.assertEqual(serialized_message['content'], "Ciao!")
